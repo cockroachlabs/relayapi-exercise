071aca8c82b4cfb0dad3086405575a2b1031d24c
Kenneth Liu <ken@cockroachlabs.com>
2021-03-03 14:09:01 -0500

    update message format and README

---
+++ splonk/readme.md
@@ -0,0 +1,6 @@
+This is the source code for the "Splonk" server for the exercise. This is not part of the candidate exercise, the source code is here as a reference.
+
+# local testing (development)
+
+`bundle exec functions-framework-ruby --source=splonk.rb --target=event`
+`curl -X POST -H "Content-Type: application/json" -d @event.json http://localhost:8080`
--- splonk/splonk.rb
+++ splonk/splonk.rb
@@ -6,8 +6,9 @@ require "pry"
 # expected event format:
 # {
 #   "timestamp": 1437522387,
+#   "user": "admin@example.com",
+#   "sourceIP": "108.50.169.163",
 #   "event": {
-#     "sourceIP": "108.50.169.163",
 #     "clusterID": "26b3c8ef50bf96c2",
 #     "action": "take",
 #     "timestamp": 1437522387 
@@ -15,12 +16,18 @@ require "pry"
 # }
 # 
 
+def user? email
+  email != nil # TODO validate email addr format
+end
+
 def timestamp? time
   time != nil
 end
 
 def sourceip? ip
-  ip != nil # TODO regex format
+  block = /\d{,2}|1\d{2}|2[0-4]\d|25[0-5]/
+  re = /\A#{block}\.#{block}\.#{block}\.#{block}\z/
+  ip != nil && re =~ ip  
 end
 
 def clusterid? id
@@ -32,18 +39,27 @@ def action? action
 end 
 
 FunctionsFramework.http("event") do |request|
+  puts "received event"
   good_event = false
   if request.post?
     event = request.POST
     valid_event = false
     body = JSON.parse(request.body.read) # TODO handle really badly formatted event
 
+    # validate top-level fields
+    if not(timestamp?(body["timestamp"]) && sourceip?(body["sourceIP"]) && user?(body["user"]))
+      puts "badly formatted message"
+      break
+    end
+
     puts body
-    if body["timestamp"] != nil && body["event"] != nil
-      event = body["event"]
-      if (timestamp?(event["timestamp"]) && sourceip?(event["sourceIP"]) && clusterid?(event["clusterID"]) && action?(event["action"]))
-        good_event = true
-      end
+
+    # validate event fields
+    event = body["event"]
+    if (timestamp?(event["timestamp"]) && clusterid?(event["clusterID"]) && action?(event["action"]))
+      good_event = true
+    else 
+      puts "badly formatted event"
     end
   end
 
